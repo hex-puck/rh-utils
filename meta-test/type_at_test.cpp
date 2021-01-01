3afadf66e866728719ac4cf6d81e72a8a42eeeb2
Heron Hu <spectre.hhr@outlook.com>
2021-01-01 23:33:00 +0800

    fix typenames in type_at related files

--- meta-test/type_at_test.cpp
+++ meta-test/type_at_test.cpp
@@ -9,12 +9,11 @@ void meta_test::type_at_test() noexcept
 
     using std::is_same;
     using namespace rh_utils::meta;
-    static_assert(is_same<void, type_at_index_t<0, void>>::value, "should be true");
-    static_assert(is_same<void, type_at_index_t<0, void, int, double>>::value, "should be true");
-    static_assert(is_same<int, type_at_index_t<1, void, int, double>>::value, "should be true");
-    static_assert(is_same<double, type_at_index_t<2, void, int, double>>::value, "should be true");
-    static_assert(is_same<char, type_at_index_t<0, char, unsigned char, signed char >>::value, "should be true");
-    static_assert(is_same<unsigned char, type_at_index_t<1, char, unsigned char, signed char >>::value,
-                  "should be true");
-    static_assert(is_same<signed char, type_at_index_t<2, char, unsigned char, signed char >>::value, "should be true");
+    static_assert(is_same<void, type_at_t<0, void>>::value, "should be true");
+    static_assert(is_same<void, type_at_t<0, void, int, double>>::value, "should be true");
+    static_assert(is_same<int, type_at_t<1, void, int, double>>::value, "should be true");
+    static_assert(is_same<double, type_at_t<2, void, int, double>>::value, "should be true");
+    static_assert(is_same<char, type_at_t<0, char, unsigned char, signed char >>::value, "should be true");
+    static_assert(is_same<unsigned char, type_at_t<1, char, unsigned char, signed char >>::value, "should be true");
+    static_assert(is_same<signed char, type_at_t<2, char, unsigned char, signed char >>::value, "should be true");
 }
--- meta/type_at.h
+++ meta/type_at.h
@@ -1,22 +1,24 @@
 #ifndef RH_UTILS_TYPE_AT_H
 #define RH_UTILS_TYPE_AT_H
 
+#include <cstddef>
+
 namespace rh_utils {
     namespace meta {
         template<std::size_t I, typename... Args>
-        struct type_at_index;
+        struct type_at;
 
         template<std::size_t I, typename... Args>
-        using type_at_index_t = typename type_at_index<I, Args...>::type;
+        using type_at_t = typename type_at<I, Args...>::type;
 
         template<typename T, typename... Args>
-        struct type_at_index<0, T, Args...> { using type = T; };
+        struct type_at<0, T, Args...> { using type = T; };
 
         template<std::size_t I, typename T, typename... Args>
-        struct type_at_index<I, T, Args...>
+        struct type_at<I, T, Args...>
         {
             static_assert(I < sizeof...(Args) + 1, "index is too large");
-            using type = type_at_index_t<I - 1, Args...>;
+            using type = type_at_t<I - 1, Args...>;
         };
     }
 }
