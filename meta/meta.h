3bfc19b39b72b62bbcc1cfd8620a37f3d4c8e003
Heron Hu <spectre.hhr@outlook.com>
2021-01-03 21:15:27 +0800

    make in_range accessible in meta.h. fix compile error of in_range.

--- meta/meta.h
+++ meta/meta.h
@@ -1,7 +1,8 @@
 #ifndef RH_UTILS_META_H
 #define RH_UTILS_META_H
 
-#include <rh-utils/meta/type_sequence.h>
+#include <rh-utils/meta/in_range.h>
 #include <rh-utils/meta/type_at.h>
+#include <rh-utils/meta/type_sequence.h>
 
 #endif //RH_UTILS_META_H
