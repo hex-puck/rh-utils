ee0c82022b1e1669228e686cdda0bed931043a08
Heron Hu <spectre.hhr@outlook.com>
2021-01-03 21:55:14 +0800

    add test for unsigned integer type in_range invocation

--- meta-test/in_range_test.cpp
+++ meta-test/in_range_test.cpp
@@ -11,18 +11,35 @@ TEST(in_range, same_type_int)
     using namespace rh_utils::meta;
 
     EXPECT_TRUE(in_range<int>(nl<int>::lowest()));
-    EXPECT_TRUE(in_range<int>((int)0));
+    EXPECT_TRUE(in_range<int>((int) 0));
     EXPECT_TRUE(in_range<int>(nl<int>::max()));
 
     EXPECT_TRUE(in_range<double>(nl<double>::lowest()));
-    EXPECT_TRUE(in_range<double>((double)0));
+    EXPECT_TRUE(in_range<double>((double) 0));
     EXPECT_TRUE(in_range<double>(nl<double>::max()));
 
     EXPECT_TRUE(in_range<short>(nl<short>::lowest()));
-    EXPECT_TRUE(in_range<short>((short)0));
+    EXPECT_TRUE(in_range<short>((short) 0));
     EXPECT_TRUE(in_range<short>(nl<short>::max()));
 
     EXPECT_TRUE(in_range<float>(nl<float>::lowest()));
-    EXPECT_TRUE(in_range<float>((float)0));
+    EXPECT_TRUE(in_range<float>((float) 0));
     EXPECT_TRUE(in_range<float>(nl<float>::max()));
+}
+
+TEST(in_range, integral_unsigned_unsigned)
+{
+    using namespace rh_utils::meta;
+
+    // equal size
+    EXPECT_TRUE(in_range<std::uint32_t>(nl<std::uint32_t>::max()));
+    EXPECT_TRUE(in_range<std::uint32_t>(nl<std::uint32_t>::lowest()));
+
+    // small -> big
+    EXPECT_TRUE(in_range<std::uint32_t>(nl<std::uint8_t>::max()));
+    EXPECT_TRUE(in_range<std::uint32_t>(nl<std::uint8_t>::lowest()));
+
+    // big -> small
+    EXPECT_FALSE(in_range<std::uint8_t>(nl<std::uint32_t>::max()));
+    EXPECT_TRUE(in_range<std::uint8_t>(nl<std::uint32_t>::lowest()));
 }
