08169a6a85ce7770d2a9f1ff3ce178b9c317de02
Heron Hu <spectre.hhr@outlook.com>
2021-01-03 21:46:18 +0800

    add test of same type in_range invocation

--- meta-test/in_range_test.cpp
+++ meta-test/in_range_test.cpp
@@ -1,6 +1,28 @@
 #include <gtest/gtest.h>
 #include <rh-utils/meta.h>
 
-TEST(in_range, usage)
+namespace {
+    template<typename T>
+    using nl = std::numeric_limits<T>;
+}
+
+TEST(in_range, same_type_int)
 {
+    using namespace rh_utils::meta;
+
+    EXPECT_TRUE(in_range<int>(nl<int>::lowest()));
+    EXPECT_TRUE(in_range<int>((int)0));
+    EXPECT_TRUE(in_range<int>(nl<int>::max()));
+
+    EXPECT_TRUE(in_range<double>(nl<double>::lowest()));
+    EXPECT_TRUE(in_range<double>((double)0));
+    EXPECT_TRUE(in_range<double>(nl<double>::max()));
+
+    EXPECT_TRUE(in_range<short>(nl<short>::lowest()));
+    EXPECT_TRUE(in_range<short>((short)0));
+    EXPECT_TRUE(in_range<short>(nl<short>::max()));
+
+    EXPECT_TRUE(in_range<float>(nl<float>::lowest()));
+    EXPECT_TRUE(in_range<float>((float)0));
+    EXPECT_TRUE(in_range<float>(nl<float>::max()));
 }
