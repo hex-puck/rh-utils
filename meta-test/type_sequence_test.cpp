a580fa38c194ffcfc834ddf375af1ea2ec7bea1e
Heron Hu <spectre.hhr@outlook.com>
2021-01-04 20:49:25 +0800

    modify test method for copile test

--- meta-test/type_sequence_test.cpp
+++ meta-test/type_sequence_test.cpp
@@ -6,19 +6,20 @@ TEST(type_sequence, usage)
 {
     using std::is_same;
     using namespace rh_utils::meta;
-    static_assert(type_sequence<>::size() == 0, "should be true");
-    static_assert(type_sequence<void>::size() == 1, "should be true");
-    static_assert(type_sequence<int>::size() == 1, "should be true");
-    static_assert(type_sequence<void, void>::size() == 2, "should be true");
-    static_assert(type_sequence<void, int>::size() == 2, "should be true");
+
+    EXPECT_TRUE((type_sequence<>::size() == 0));
+    EXPECT_TRUE((type_sequence<void>::size() == 1));
+    EXPECT_TRUE((type_sequence<int>::size() == 1));
+    EXPECT_TRUE((type_sequence<void, void>::size() == 2));
+    EXPECT_TRUE((type_sequence<void, int>::size() == 2));
 
     using test_seq_1 = type_sequence<void, int, double>;
-    static_assert(is_same<void, typename test_seq_1::type<0>>::value, "should be true");
-    static_assert(is_same<int, typename test_seq_1::type<1>>::value, "should be true");
-    static_assert(is_same<double, typename test_seq_1::type<2>>::value, "should be true");
+    EXPECT_TRUE((is_same<void, typename test_seq_1::type<0>>::value));
+    EXPECT_TRUE((is_same<int, typename test_seq_1::type<1>>::value));
+    EXPECT_TRUE((is_same<double, typename test_seq_1::type<2>>::value));
 
     using test_seq_2 = type_sequence<char, unsigned char, signed char>;
-    static_assert(is_same<char, typename test_seq_2::type<0>>::value, "should be true");
-    static_assert(is_same<unsigned char, typename test_seq_2::type<1>>::value, "should be true");
-    static_assert(is_same<signed char, typename test_seq_2::type<2>>::value, "should be true");
+    EXPECT_TRUE((is_same<char, typename test_seq_2::type<0>>::value));
+    EXPECT_TRUE((is_same<unsigned char, typename test_seq_2::type<1>>::value));
+    EXPECT_TRUE((is_same<signed char, typename test_seq_2::type<2>>::value));
 }
