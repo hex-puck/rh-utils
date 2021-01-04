097b4bd94c574933a318ea30d984be0a94717401
Heron Hu <spectre.hhr@outlook.com>
2021-01-04 20:49:58 +0800

    add function_info type

--- meta/meta.h
+++ meta/meta.h
@@ -1,6 +1,7 @@
 #ifndef RH_UTILS_META_H
 #define RH_UTILS_META_H
 
+#include <rh-utils/meta/function_info.h>
 #include <rh-utils/meta/in_range.h>
 #include <rh-utils/meta/type_at.h>
 #include <rh-utils/meta/type_sequence.h>
