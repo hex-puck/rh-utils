ca31fe4ea0a31f04c31673046bbac1f95e6cad0c
Heron Hu <spectre.hhr@outlook.com>
2021-01-09 12:24:19 +0800

    add unit test for non-static member function function_info

--- meta-test/function_info_test.cpp
+++ meta-test/function_info_test.cpp
@@ -7,10 +7,28 @@ TEST(function_info, usage)
     using namespace rh_utils::meta;
     using std::is_same;
 
-    using func_type = void(int, double);
-    using info_type = function_info<func_type>;
+    using func_t = void(int, double);
+    using info_t = function_info<func_t>;
 
-    EXPECT_TRUE((is_same<func_type, typename info_type::type>::value));
-    EXPECT_TRUE((is_same<void, typename info_type::return_type>::value));
-    EXPECT_TRUE((is_same<type_sequence<int, double>, typename info_type::parameter_sequence>::value));
+    EXPECT_TRUE((is_same<func_t, typename info_t::type>::value));
+    EXPECT_TRUE((is_same<void, typename info_t::return_type>::value));
+    EXPECT_TRUE((is_same<type_sequence<int, double>, typename info_t::parameter_sequence>::value));
+}
+
+TEST(function_info, mem_func)
+{
+    using namespace rh_utils::meta;
+    using std::is_same;
+
+    struct foo { void print(int, double); };
+    using class_t = foo;
+    using mem_func_t = void (foo::*)(int, double);
+    using bare_func_t = void(int, double);
+    using info_t = function_info<decltype(&foo::print)>;
+
+    EXPECT_TRUE((std::is_same<mem_func_t, info_t::type>::value));
+    EXPECT_TRUE((std::is_same<class_t, info_t::class_type>::value));
+    EXPECT_TRUE((std::is_same<bare_func_t, info_t::bare_type>::value));
+    EXPECT_TRUE((is_same<void, typename info_t::return_type>::value));
+    EXPECT_TRUE((is_same<type_sequence<int, double>, typename info_t::parameter_sequence>::value));
 }
