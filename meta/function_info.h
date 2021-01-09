fa90e21df4431ebf54fd96a478979bfba050065e
Heron Hu <spectre.hhr@outlook.com>
2021-01-09 14:36:39 +0800

    make function_info support for other non-static member function

--- meta/function_info.h
+++ meta/function_info.h
@@ -29,6 +29,179 @@ namespace rh_utils {
             using type = R(C::*)(std::decay_t<Args>...);
             using bare_type = typename function_info<R(Args...)>::type;
         };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) volatile>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) volatile;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const volatile>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const volatile;
+        };
+
+#if RH_CXX_STANDARD >= 201103L
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) &>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) &;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const &>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const &;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) volatile &>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) volatile &;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const volatile &>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const volatile &;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) &&>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) &&;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const &&>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const &&;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) volatile &&>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) volatile &&;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const volatile &&>
+                : public function_info<R(C::*)(Args...)>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const volatile &&;
+        };
+#endif
+#if RH_CXX_STANDARD >= 201703L
+        template<typename R, typename ...Args>
+        struct function_info<R(Args...) noexcept>
+                : public function_info<R(Args...)>
+        {
+            using type = R(std::decay_t<Args>...) noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) noexcept>
+                : public function_info<R(Args...) noexcept>
+        {
+            using class_type = C;
+            using type = R(C::*)(std::decay_t<Args>...) noexcept;
+            using bare_type = typename function_info<R(Args...) noexcept>::type;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) volatile noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) volatile noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const volatile noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const volatile noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) &noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) &noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const &noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const &noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) volatile &noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) volatile &noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const volatile &noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const volatile &noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) &&noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) &&noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const &&noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const &&noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) volatile &&noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) volatile &&noexcept;
+        };
+
+        template<typename C, typename R, typename ...Args>
+        struct function_info<R(C::*)(Args...) const volatile &&noexcept>
+                : public function_info<R(C::*)(Args...) noexcept>
+        {
+            using type = R(C::*)(std::decay_t<Args>...) const volatile &&noexcept;
+        };
+#endif
     }
 }
 
