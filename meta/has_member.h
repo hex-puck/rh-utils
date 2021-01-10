4d06b6fe30272ca70abf40855d76216b00abbbe4
Heron Hu <spectre.hhr@outlook.com>
2021-01-10 12:50:04 +0800

    has_member -> has_member_object

--- meta/has_member.h
+++ meta/has_member.h
@@ -5,15 +5,15 @@
 #include <utility>
 #include <rh-utils/meta/void_t.h>
 
-#define RH_HAS_MEMBER(Member)                                                                                 \
-namespace rh_utils {                                                                                          \
-    namespace meta {                                                                                          \
-        template<typename, typename = void>                                                                   \
-        struct has_member_##Member : public std::false_type {};                                               \
-                                                                                                              \
-        template<typename C>                                                                                  \
-        struct has_member_##Member<C, void_t<decltype(std::declval<C>().Member)>> : public std::true_type {}; \
-    }                                                                                                         \
+#define RH_HAS_MEMBER_OBJECT(Member)                                                                                 \
+namespace rh_utils {                                                                                                 \
+    namespace meta {                                                                                                 \
+        template<typename, typename = void>                                                                          \
+        struct has_member_object_##Member : public std::false_type {};                                               \
+                                                                                                                     \
+        template<typename C>                                                                                         \
+        struct has_member_object_##Member<C, void_t<decltype(std::declval<C>().Member)>> : public std::true_type {}; \
+    }                                                                                                                \
 }
 
 #endif //RH_UTILS_HAS_MEMBER_H
