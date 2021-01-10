866f468ae450953690ee6f25d1cf50cf5929c6b5
Heron Hu <spectre.hhr@outlook.com>
2021-01-10 12:51:40 +0800

    fix and promote for unit test for has_member

--- meta-test/has_member_test.cpp
+++ meta-test/has_member_test.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 #include <rh-utils/meta.h>
 
-RH_HAS_MEMBER(foo)
+RH_HAS_MEMBER_OBJECT(foo)
+RH_HAS_MEMBER_FUNCTION(foo)
 
 TEST(has_member, usage)
 {
@@ -10,8 +11,21 @@ TEST(has_member, usage)
     using class_t1 = struct { int foo; };
     using class_t2 = struct { void foo(); };
     using class_t3 = struct {};
+    using class_t4 = struct { void foo(); void foo() const; void foo(int); };
 
-    EXPECT_TRUE(has_member_foo<class_t1>::value);
-    EXPECT_FALSE(has_member_foo<class_t2>::value);
-    EXPECT_FALSE(has_member_foo<class_t3>::value);
+    EXPECT_TRUE(has_member_object_foo<class_t1>::value);
+    EXPECT_FALSE(has_member_object_foo<class_t2>::value);
+    EXPECT_FALSE(has_member_object_foo<class_t3>::value);
+    EXPECT_FALSE(has_member_object_foo<class_t4>::value);
+
+    EXPECT_FALSE(has_member_function_foo<class_t1>::value);
+    EXPECT_TRUE(has_member_function_foo<class_t2>::value);
+    EXPECT_FALSE(has_member_function_foo<class_t3>::value);
+    EXPECT_TRUE(has_member_function_foo<class_t4>::value);
+    EXPECT_TRUE(has_member_function_foo<const class_t4>::value);
+    EXPECT_TRUE((has_member_function_foo<class_t4, int>::value));
+    EXPECT_FALSE((has_member_function_foo<const class_t4, int>::value));
+    EXPECT_TRUE((has_member_function_foo<class_t4, double>::value));
+
+    EXPECT_FALSE((has_member_function_foo<class_t2, int>::value));
 }
