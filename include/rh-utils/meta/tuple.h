d46c21e050a1696819c4e2a39967830800dae4ee
Heron Hu <spectre.hhr@outlook.com>
2022-02-06 15:51:35 +0800

    doc: support tuple.h

--- include/rh-utils/meta/tuple.h
+++ include/rh-utils/meta/tuple.h
@@ -5,15 +5,21 @@
 
 namespace rh_utils {
 namespace meta {
+/// @brief just std::tuple
 template <typename... Args> using tuple = std::tuple<Args...>;
 
+/// @brief promoted version of std::tuple_size
 template <typename T> struct tuple_size : public std::tuple_size<T> {};
+/// @brief promoted version of std::tuple_element
 template <std::size_t I, typename T>
 struct tuple_element : public std::tuple_element<I, T> {};
+/// @brief convenience type alias of `tuple_element`
 template <std::size_t I, typename T>
 using tuple_element_t = typename tuple_element<I, T>::type;
 
+/// @brief make some type to tuple
 template <typename> struct to_tuple;
+/// @brief convenience type alias of `to_tuple`
 template <typename T> using to_tuple_t = typename to_tuple<T>::type;
 } // namespace meta
 } // namespace rh_utils
