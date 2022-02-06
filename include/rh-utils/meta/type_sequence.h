c34227bbda18bab8d03b09f4cc7e42f2ba69588a
Heron Hu <spectre.hhr@outlook.com>
2022-02-06 16:14:41 +0800

    doc: support type_sequence.h

--- include/rh-utils/meta/type_sequence.h
+++ include/rh-utils/meta/type_sequence.h
@@ -7,26 +7,49 @@
 
 namespace rh_utils {
 namespace meta {
+/**
+ *  @brief a sequence of type, like `std::integer_sequence`
+ *
+ *  tuple seems for object usage, so use type_sequence for meta-programming.
+ */
 template <typename... Args> struct type_sequence {
+  /**
+   *  @brief make a new `type_sequence`
+   *  @tparam Ints index pack of position of types which will be Args of new
+   *    type_sequence
+   */
   template <std::size_t... Ints> struct rebind {
     using other = type_sequence<type_at_t<Ints, Args...>...>;
   };
 
+  /// @brief make sizeof...(Args)
   static constexpr std::size_t size() noexcept { return sizeof...(Args); }
 
+  /**
+   *  @brief extract type at the given position
+   *  @tparam I position index
+   */
   template <std::size_t I> using type = type_at_t<I, Args...>;
 };
 
+/**
+ *  @brief make support for `type_sequence`
+ *
+ *  return type count of `type_sequence`
+ */
 template <typename... Args>
 struct tuple_size<type_sequence<Args...>>
     : std::integral_constant<std::size_t, type_sequence<Args...>::size()> {};
 
+/// @brief make support for `type_sequence`
 template <std::size_t I, typename... Args>
 struct tuple_element<I, type_sequence<Args...>> {
   using type = typename type_sequence<Args...>::template type<I>;
 };
 
+/// @brief make some type to `type_sequence`
 template <typename> struct to_type_sequence;
+/// @brief convenience type alias of `to_type_sequence`
 template <typename T>
 using to_type_sequence_t = typename to_type_sequence<T>::type;
 
@@ -34,6 +57,7 @@ template <typename... Args> struct to_type_sequence<tuple<Args...>> {
   using type = type_sequence<Args...>;
 };
 
+/// @brief make support for `type_sequence`
 template <typename... Args> struct to_tuple<type_sequence<Args...>> {
   using type = tuple<Args...>;
 };
@@ -52,13 +76,24 @@ struct type_sequence_splitter_helper<I, std::index_sequence<Ints...>,
 };
 } // namespace detail
 
+/**
+ *  @brief primary template of `type_sequence_splitter`
+ *  @note it's a no definition incomplete type.
+ */
 template <std::size_t, typename> struct type_sequence_splitter;
+/**
+ * @brief split the `type_sequence` into two sequence at given position
+ * @tparam I index of split point
+ */
 template <std::size_t I, typename... Args>
 struct type_sequence_splitter<I, type_sequence<Args...>> {
+  /// @brief original type_sequence
   using origin = type_sequence<Args...>;
+  /// @brief front-half result
   using front =
       detail::type_sequence_splitter_helper_t<0, std::make_index_sequence<I>,
-                                            origin>;
+                                              origin>;
+  /// @brief back-half result
   using back = detail::type_sequence_splitter_helper_t<
       I, std::make_index_sequence<origin::size() - I>, origin>;
 };
