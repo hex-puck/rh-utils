2575e40299795bad358bea4d9a7c05febfc74ef2
Heron Hu <spectre.hhr@outlook.com>
2022-04-04 20:53:25 +0800

    remove incomplete design `has_member` related functionality.

--- include/rh-utils/meta.h
+++ include/rh-utils/meta.h
@@ -7,7 +7,6 @@
 #define RH_UTILS_META_H
 
 #include <rh-utils/meta/function_info.h>
-#include <rh-utils/meta/has_member.h>
 #include <rh-utils/meta/in_range.h>
 #include <rh-utils/meta/tuple.h>
 #include <rh-utils/meta/type_at.h>
