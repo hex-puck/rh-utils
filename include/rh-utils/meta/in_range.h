20d2fd3d5ca404b82fd31528ffccf9a0b6c242be
Heron Hu <spectre.hhr@outlook.com>
2022-10-10 22:20:50 +0800

    tidy-up

--- include/rh-utils/meta/in_range.h
+++ include/rh-utils/meta/in_range.h
@@ -6,14 +6,74 @@
 
 namespace rh_utils {
 namespace meta {
+namespace detail {
+template <typename T, typename U> struct to_same_type : std::is_same<T, U> {};
+
+template <typename T, typename U>
+struct ui_to_ui_ge
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_unsigned<T>::value &&
+                    std::is_unsigned<U>::value && (sizeof(T) >= sizeof(U))> {};
+
+template <typename T, typename U>
+struct ui_to_ui_lt
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_unsigned<T>::value &&
+                    std::is_unsigned<U>::value && (sizeof(T) < sizeof(U))> {};
+
+template <typename T, typename U>
+struct si_to_si_ge
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_signed<T>::value &&
+                    std::is_signed<U>::value && (sizeof(T) >= sizeof(U))> {};
+
+template <typename T, typename U>
+struct si_to_si_lt
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_signed<T>::value &&
+                    std::is_signed<U>::value && (sizeof(T) < sizeof(U))> {};
+
+template <typename T, typename U>
+struct si_to_ui_ge
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_unsigned<T>::value &&
+                    std::is_signed<U>::value && (sizeof(T) >= sizeof(U))> {};
+
+template <typename T, typename U>
+struct si_to_ui_lt
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_unsigned<T>::value &&
+                    std::is_signed<U>::value && (sizeof(T) < sizeof(U))> {};
+
+template <typename T, typename U>
+struct ui_to_si_gt
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_signed<T>::value &&
+                    std::is_unsigned<U>::value && (sizeof(T) > sizeof(U))> {};
+
+template <typename T, typename U>
+struct ui_to_si_le
+    : std::integral_constant<
+          bool, !std::is_same<T, U>::value && std::is_integral<T>::value &&
+                    std::is_integral<U>::value && std::is_signed<T>::value &&
+                    std::is_unsigned<U>::value && (sizeof(T) <= sizeof(U))> {};
+} // namespace detail
+
 /**
  *  @brief judge is or not in_range when same type
  *  @tparam T target type
  *  @tparam U original type
  */
-template <typename T, typename U,
-          std::enable_if_t<std::is_same<T, U>::value> * = nullptr>
-constexpr bool in_range(U /*v*/) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::to_same_type<T, U>::value, bool>
+in_range(U /*v*/) noexcept {
   return true;
 }
 
@@ -23,13 +83,9 @@ constexpr bool in_range(U /*v*/) noexcept {
  *  @tparam T target type, should be unsigned integer type
  *  @tparam U original type, should be unsigned integer type
  */
-template <
-    typename T, typename U,
-    std::enable_if_t<!std::is_same<T, U>::value && std::is_integral<T>::value &&
-                     std::is_integral<U>::value && std::is_unsigned<T>::value &&
-                     std::is_unsigned<U>::value && (sizeof(T) >= sizeof(U))> * =
-        nullptr>
-constexpr bool in_range(U /*v*/) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::ui_to_ui_ge<T, U>::value, bool>
+in_range(U /*v*/) noexcept {
   static_assert(std::numeric_limits<T>::lowest() <=
                     std::numeric_limits<U>::lowest(),
                 "should be true");
@@ -44,13 +100,9 @@ constexpr bool in_range(U /*v*/) noexcept {
  *  @tparam T target type, should be unsigned integer type
  *  @tparam U original type, should be unsigned integer type
  */
-template <
-    typename T, typename U,
-    std::enable_if_t<!std::is_same<T, U>::value && std::is_integral<T>::value &&
-                     std::is_integral<U>::value && std::is_unsigned<T>::value &&
-                     std::is_unsigned<U>::value && (sizeof(T) < sizeof(U))> * =
-        nullptr>
-constexpr bool in_range(U v) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::ui_to_ui_lt<T, U>::value, bool>
+in_range(U v) noexcept {
   static_assert(std::numeric_limits<T>::lowest() <=
                     std::numeric_limits<U>::lowest(),
                 "should be true");
@@ -63,12 +115,9 @@ constexpr bool in_range(U v) noexcept {
  *  @tparam T target type, should be signed integer type
  *  @tparam U original type, should be signed integer type
  */
-template <typename T, typename U,
-          std::enable_if_t<
-              !std::is_same<T, U>::value && std::is_integral<T>::value &&
-              std::is_integral<U>::value && std::is_signed<T>::value &&
-              std::is_signed<U>::value && (sizeof(T) >= sizeof(U))> * = nullptr>
-constexpr bool in_range(U /*v*/) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::si_to_si_ge<T, U>::value, bool>
+in_range(U /*v*/) noexcept {
   static_assert(std::numeric_limits<T>::lowest() <=
                     std::numeric_limits<U>::lowest(),
                 "should be true");
@@ -83,12 +132,9 @@ constexpr bool in_range(U /*v*/) noexcept {
  *  @tparam T target type, should be signed integer type
  *  @tparam U original type, should be signed integer type
  */
-template <typename T, typename U,
-          std::enable_if_t<
-              !std::is_same<T, U>::value && std::is_integral<T>::value &&
-              std::is_integral<U>::value && std::is_signed<T>::value &&
-              std::is_signed<U>::value && (sizeof(T) < sizeof(U))> * = nullptr>
-constexpr bool in_range(U v) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::si_to_si_lt<T, U>::value, bool>
+in_range(U v) noexcept {
   return std::numeric_limits<T>::lowest() <= v &&
          v <= std::numeric_limits<T>::max();
 }
@@ -99,12 +145,9 @@ constexpr bool in_range(U v) noexcept {
  *  @tparam T target type, should be unsigned integer type
  *  @tparam U original type, should be signed integer type
  */
-template <typename T, typename U,
-          std::enable_if_t<
-              !std::is_same<T, U>::value && std::is_integral<T>::value &&
-              std::is_integral<U>::value && std::is_unsigned<T>::value &&
-              std::is_signed<U>::value && (sizeof(T) >= sizeof(U))> * = nullptr>
-constexpr bool in_range(U v) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::si_to_ui_ge<T, U>::value, bool>
+in_range(U v) noexcept {
   static_assert(
       static_cast<std::make_unsigned_t<U>>(std::numeric_limits<U>::max()) <=
           std::numeric_limits<T>::max(),
@@ -119,12 +162,9 @@ constexpr bool in_range(U v) noexcept {
  *  @tparam T target type, should be unsigned integer type
  *  @tparam U original type, should be signed integer type
  */
-template <typename T, typename U,
-          std::enable_if_t<
-              !std::is_same<T, U>::value && std::is_integral<T>::value &&
-              std::is_integral<U>::value && std::is_unsigned<T>::value &&
-              std::is_signed<U>::value && (sizeof(T) < sizeof(U))> * = nullptr>
-constexpr bool in_range(U v) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::si_to_ui_lt<T, U>::value, bool>
+in_range(U v) noexcept {
   if (static_cast<std::make_signed_t<T>>(std::numeric_limits<T>::lowest()) <=
       v) {
     return static_cast<std::make_unsigned_t<U>>(v) <=
@@ -139,13 +179,9 @@ constexpr bool in_range(U v) noexcept {
  *  @tparam T target type, should be signed integer type
  *  @tparam U original type, should be unsigned integer type
  */
-template <
-    typename T, typename U,
-    std::enable_if_t<!std::is_same<T, U>::value && std::is_integral<T>::value &&
-                     std::is_integral<U>::value && std::is_signed<T>::value &&
-                     std::is_unsigned<U>::value && (sizeof(T) > sizeof(U))> * =
-        nullptr>
-constexpr bool in_range(U /*v*/) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::ui_to_si_gt<T, U>::value, bool>
+in_range(U /*v*/) noexcept {
   static_assert(
       std::numeric_limits<T>::lowest() <=
           static_cast<std::make_signed_t<U>>(std::numeric_limits<U>::lowest()),
@@ -163,13 +199,9 @@ constexpr bool in_range(U /*v*/) noexcept {
  *  @tparam T target type, should be signed integer type
  *  @tparam U original type, should be unsigned integer type
  */
-template <
-    typename T, typename U,
-    std::enable_if_t<!std::is_same<T, U>::value && std::is_integral<T>::value &&
-                     std::is_integral<U>::value && std::is_signed<T>::value &&
-                     std::is_unsigned<U>::value && (sizeof(T) <= sizeof(U))> * =
-        nullptr>
-constexpr bool in_range(U v) noexcept {
+template <typename T, typename U>
+constexpr std::enable_if_t<detail::ui_to_si_le<T, U>::value, bool>
+in_range(U v) noexcept {
   static_assert(
       std::numeric_limits<T>::lowest() <=
           static_cast<std::make_signed_t<U>>(std::numeric_limits<U>::lowest()),
